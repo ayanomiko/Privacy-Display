707a1f87659db38607e2c50b73636d9770abfa62
ayanomiko <163816941+ayanomiko@users.noreply.github.com>
2026-05-16 08:01:19 +0700

    Update MainActivity.java

--- android/app/src/main/java/com/privacydisplay/app/MainActivity.java
+++ android/app/src/main/java/com/privacydisplay/app/MainActivity.java
@@ -1,5 +1,46 @@
 package com.privacydisplay.app;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.Settings;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    private static final int OVERLAY_PERMISSION_CODE = 1001;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(PrivacyDisplayPlugin.class);
+        super.onCreate(savedInstanceState);
+        requestOverlayPermission();
+    }
+
+    private void requestOverlayPermission() {
+        if (!Settings.canDrawOverlays(this)) {
+            Intent intent = new Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName())
+            );
+            startActivityForResult(intent, OVERLAY_PERMISSION_CODE);
+        } else {
+            startPrivacyService();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == OVERLAY_PERMISSION_CODE) {
+            if (Settings.canDrawOverlays(this)) {
+                startPrivacyService();
+            }
+        }
+    }
+
+    private void startPrivacyService() {
+        Intent intent = new Intent(this, PrivacyService.class);
+        startForegroundService(intent);
+    }
+}
